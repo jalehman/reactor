44d166a5dfbd5853085a7bd64f83a987149990d9
Josh Lehman <jalehman37@gmail.com>
2017-07-07 11:38:46 -0700

    remove unused code

--- src/reactor/handlers/stripe.clj
+++ src/reactor/handlers/stripe.clj
@@ -1,25 +1,6 @@
 (ns reactor.handlers.stripe
   "Only serves to `require` the Stripe handlers distributed across multiple
   namespaces."
-  (:require [clojure
-             [spec :as s]
-             [string :as string]]
-            [reactor.handlers.stripe.charge]
+  (:require [reactor.handlers.stripe.charge]
             [reactor.handlers.stripe.customer]
             [reactor.handlers.stripe.invoice]))
-
-
-(defn- snake->kebab [s]
-  (string/replace s #"_" "-"))
-
-
-(defn- event-type->key [et]
-  (let [parts (->> (concat '("stripe" "event") (string/split et #"\."))
-                   (map snake->kebab))]
-    (keyword
-     (string/join "." (butlast parts))
-     (last parts))))
-
-(s/fdef event-type->key
-        :args (s/cat :type string?)
-        :ret keyword?)
