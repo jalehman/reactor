d7753712033c8681303dec9aeaa1790ef1fd3924
Josh Lehman <jalehman37@gmail.com>
2018-05-01 08:00:06 -0700

    Version 1.9.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject starcity/reactor "1.9.2"
+(defproject starcity/reactor "1.9.3-SNAPSHOT"
   :description "Transactional event processing queue."
   :url "https://github.com/starcity-properties/reactor"
   :license {:name "Eclipse Public License"
