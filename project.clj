d233ad07b900819a988fef289cce11b36369c239
Allen J Price <allenjprice@gmail.com>
2018-07-17 16:37:33 -0700

    Version 1.12.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject starcity/reactor "1.12.2-SNAPSHOT"
+(defproject starcity/reactor "1.12.2"
   :description "Transactional event processing queue."
   :url "https://github.com/starcity-properties/reactor"
   :license {:name "Eclipse Public License"
